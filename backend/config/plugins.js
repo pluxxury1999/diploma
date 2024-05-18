bfb57fb3b946b8db45f4b53528aea2ec9a9b1069
pluxxury <pedobirandfriends@gmail.com>
2024-05-19 00:02:44 +0300

    added random-sort plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,15 +1,17 @@
 module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'pedobirandfriends@gmail.com',
-          defaultReplyTo: 'pedobirandfriends@gmail.com',
-        },
+  "random-sort": {
+    enabled: true,
+  },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "pedobirandfriends@gmail.com",
+        defaultReplyTo: "pedobirandfriends@gmail.com",
       },
     },
-  });
-  
+  },
+});
--- backend/src/api/word/controllers/word.js
+++ backend/src/api/word/controllers/word.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * word controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::word.word');
